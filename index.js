f918f57d55814a37baf741e58b8cd5ef4734a40f
Gustavo Souza <gustavoelia7@gmail.com>
2024-06-24 20:03:21 -0300

    connecting to mongodb with the mongoose library

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const uri = `mongodb+srv://gustavoelia7:Nj9oRYR20fXX8WuF@backenddb.0pr5xba.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`;
 const port = process.env.PORT || 8000;
+
 
 app.get('/', (req, res) => {
     res.status(200).send('server running!');
 });
 
-app.listen(port, 'localhost', () => {
-    console.log(`server running on http://localhost:${port}`)
-});
+mongoose.connect(uri)
+    .then(() => {
+        console.log('Connected to database');
+        app.listen(port, 'localhost', () => {
+            console.log(`Server running on http://localhost:${port}`);
+        });
+    })
+    .catch(() => console.log('Connection to database failed'));
