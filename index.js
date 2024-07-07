0370d1a4b297f1f98487325b780244d854e09601
Gustavo Souza <gustavoelia7@gmail.com>
2024-07-07 13:25:41 -0300

    connecting to mongodb with environment variables

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectToDatabase = require('./src/database/database');
 const Product = require('./models/product.model');
-const app = express();
 
-const uri = `mongodb+srv://gustavoelia7:Nj9oRYR20fXX8WuF@backenddb.0pr5xba.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`;
+const app = express();
 const port = process.env.PORT || 8000;
+
+dotenv.config();
+connectToDatabase();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('server running!');
+    res.status(200).json({ message: 'server running!' });
 });
 
 app.post('/api/products', async (req, res) => {
@@ -21,13 +24,6 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
-mongoose.connect(uri)
-    .then(() => {
-        console.log('Connected to database');
-        app.listen(port, 'localhost', () => {
-            console.log(`Server running on http://localhost:${port}`);
-        });
-    })
-    .catch(() => {
-        console.log('Connection to database failed')
-    });
+app.listen(port, 'localhost', () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+    const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mycluster.eicxgej.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=MyCluster`;
+    
+    await mongoose.connect(uri)
+        .then(() => {
+            console.log('Connected to database');
+        })
+        .catch(() => {
+            console.log('Connection to database failed')
+        });
+}
+
+module.exports = connectToDatabase;
